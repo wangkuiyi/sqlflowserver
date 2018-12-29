763c415305469f2b83945b53acd12d42fae4ad83
Yang Yang <yzhdoudou@gmail.com>
2018-12-28 16:56:09 -0800

    change runresponse to oneof

--- sqlflow.pb.go
+++ sqlflow.pb.go
@@ -69,23 +69,25 @@ func (m *RunRequest) GetSql() string {
 
 // SQL statements like `USE database` returns only a success message.
 // In such cases, the return stream contains only one RunResponse with
-// RunResponse.message set.
+// RunResponse.messages set.
 //
 // SQL statements like `SELECT ...` returns a table in addition
-// to the status, and the table might be big.  In such cases, Run returns a
+// to the status, and the table might be big. In such cases, Run returns a
 // stream of RunResponse
 //
-// SQL statement like `SELECT ... TRAIN/PREDICT ...` returns a stream of message
+// SQL statement like `SELECT ... TRAIN/PREDICT ...` returns a stream of messages
 // which indicates the training/predicting progress
 //
 // Each RunResponse doesn't exceed the maximum message size
 // (https://stackoverflow.com/a/34186672/724872).
 type RunResponse struct {
-	Message              []string           `protobuf:"bytes,1,rep,name=message,proto3" json:"message,omitempty"`
-	Columns              map[string]*Column `protobuf:"bytes,2,rep,name=columns,proto3" json:"columns,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
-	XXX_NoUnkeyedLiteral struct{}           `json:"-"`
-	XXX_unrecognized     []byte             `json:"-"`
-	XXX_sizecache        int32              `json:"-"`
+	// Types that are valid to be assigned to Response:
+	//	*RunResponse_Messages
+	//	*RunResponse_Columns
+	Response             isRunResponse_Response `protobuf_oneof:"response"`
+	XXX_NoUnkeyedLiteral struct{}               `json:"-"`
+	XXX_unrecognized     []byte                 `json:"-"`
+	XXX_sizecache        int32                  `json:"-"`
 }
 
 func (m *RunResponse) Reset()         { *m = RunResponse{} }
@@ -113,14 +115,123 @@ func (m *RunResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_RunResponse proto.InternalMessageInfo
 
-func (m *RunResponse) GetMessage() []string {
+type isRunResponse_Response interface {
+	isRunResponse_Response()
+}
+
+type RunResponse_Messages struct {
+	Messages *Messages `protobuf:"bytes,1,opt,name=messages,proto3,oneof"`
+}
+
+type RunResponse_Columns struct {
+	Columns *Columns `protobuf:"bytes,2,opt,name=columns,proto3,oneof"`
+}
+
+func (*RunResponse_Messages) isRunResponse_Response() {}
+
+func (*RunResponse_Columns) isRunResponse_Response() {}
+
+func (m *RunResponse) GetResponse() isRunResponse_Response {
 	if m != nil {
-		return m.Message
+		return m.Response
 	}
 	return nil
 }
 
-func (m *RunResponse) GetColumns() map[string]*Column {
+func (m *RunResponse) GetMessages() *Messages {
+	if x, ok := m.GetResponse().(*RunResponse_Messages); ok {
+		return x.Messages
+	}
+	return nil
+}
+
+func (m *RunResponse) GetColumns() *Columns {
+	if x, ok := m.GetResponse().(*RunResponse_Columns); ok {
+		return x.Columns
+	}
+	return nil
+}
+
+// XXX_OneofWrappers is for the internal use of the proto package.
+func (*RunResponse) XXX_OneofWrappers() []interface{} {
+	return []interface{}{
+		(*RunResponse_Messages)(nil),
+		(*RunResponse_Columns)(nil),
+	}
+}
+
+type Messages struct {
+	Messages             []string `protobuf:"bytes,1,rep,name=messages,proto3" json:"messages,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Messages) Reset()         { *m = Messages{} }
+func (m *Messages) String() string { return proto.CompactTextString(m) }
+func (*Messages) ProtoMessage()    {}
+func (*Messages) Descriptor() ([]byte, []int) {
+	return fileDescriptor_6e9f9b081a5698ac, []int{2}
+}
+
+func (m *Messages) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Messages.Unmarshal(m, b)
+}
+func (m *Messages) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Messages.Marshal(b, m, deterministic)
+}
+func (m *Messages) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Messages.Merge(m, src)
+}
+func (m *Messages) XXX_Size() int {
+	return xxx_messageInfo_Messages.Size(m)
+}
+func (m *Messages) XXX_DiscardUnknown() {
+	xxx_messageInfo_Messages.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Messages proto.InternalMessageInfo
+
+func (m *Messages) GetMessages() []string {
+	if m != nil {
+		return m.Messages
+	}
+	return nil
+}
+
+type Columns struct {
+	Columns              map[string]*Columns_Column `protobuf:"bytes,1,rep,name=columns,proto3" json:"columns,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
+	XXX_NoUnkeyedLiteral struct{}                   `json:"-"`
+	XXX_unrecognized     []byte                     `json:"-"`
+	XXX_sizecache        int32                      `json:"-"`
+}
+
+func (m *Columns) Reset()         { *m = Columns{} }
+func (m *Columns) String() string { return proto.CompactTextString(m) }
+func (*Columns) ProtoMessage()    {}
+func (*Columns) Descriptor() ([]byte, []int) {
+	return fileDescriptor_6e9f9b081a5698ac, []int{3}
+}
+
+func (m *Columns) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Columns.Unmarshal(m, b)
+}
+func (m *Columns) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Columns.Marshal(b, m, deterministic)
+}
+func (m *Columns) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Columns.Merge(m, src)
+}
+func (m *Columns) XXX_Size() int {
+	return xxx_messageInfo_Columns.Size(m)
+}
+func (m *Columns) XXX_DiscardUnknown() {
+	xxx_messageInfo_Columns.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Columns proto.InternalMessageInfo
+
+func (m *Columns) GetColumns() map[string]*Columns_Column {
 	if m != nil {
 		return m.Columns
 	}
@@ -128,39 +239,39 @@ func (m *RunResponse) GetColumns() map[string]*Column {
 }
 
 // A data column of any type
-type Column struct {
+type Columns_Column struct {
 	Data                 []*any.Any `protobuf:"bytes,1,rep,name=data,proto3" json:"data,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}   `json:"-"`
 	XXX_unrecognized     []byte     `json:"-"`
 	XXX_sizecache        int32      `json:"-"`
 }
 
-func (m *Column) Reset()         { *m = Column{} }
-func (m *Column) String() string { return proto.CompactTextString(m) }
-func (*Column) ProtoMessage()    {}
-func (*Column) Descriptor() ([]byte, []int) {
-	return fileDescriptor_6e9f9b081a5698ac, []int{2}
+func (m *Columns_Column) Reset()         { *m = Columns_Column{} }
+func (m *Columns_Column) String() string { return proto.CompactTextString(m) }
+func (*Columns_Column) ProtoMessage()    {}
+func (*Columns_Column) Descriptor() ([]byte, []int) {
+	return fileDescriptor_6e9f9b081a5698ac, []int{3, 1}
 }
 
-func (m *Column) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Column.Unmarshal(m, b)
+func (m *Columns_Column) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Columns_Column.Unmarshal(m, b)
 }
-func (m *Column) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Column.Marshal(b, m, deterministic)
+func (m *Columns_Column) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Columns_Column.Marshal(b, m, deterministic)
 }
-func (m *Column) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Column.Merge(m, src)
+func (m *Columns_Column) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Columns_Column.Merge(m, src)
 }
-func (m *Column) XXX_Size() int {
-	return xxx_messageInfo_Column.Size(m)
+func (m *Columns_Column) XXX_Size() int {
+	return xxx_messageInfo_Columns_Column.Size(m)
 }
-func (m *Column) XXX_DiscardUnknown() {
-	xxx_messageInfo_Column.DiscardUnknown(m)
+func (m *Columns_Column) XXX_DiscardUnknown() {
+	xxx_messageInfo_Columns_Column.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_Column proto.InternalMessageInfo
+var xxx_messageInfo_Columns_Column proto.InternalMessageInfo
 
-func (m *Column) GetData() []*any.Any {
+func (m *Columns_Column) GetData() []*any.Any {
 	if m != nil {
 		return m.Data
 	}
@@ -170,31 +281,36 @@ func (m *Column) GetData() []*any.Any {
 func init() {
 	proto.RegisterType((*RunRequest)(nil), "sqlflow.RunRequest")
 	proto.RegisterType((*RunResponse)(nil), "sqlflow.RunResponse")
-	proto.RegisterMapType((map[string]*Column)(nil), "sqlflow.RunResponse.ColumnsEntry")
-	proto.RegisterType((*Column)(nil), "sqlflow.Column")
+	proto.RegisterType((*Messages)(nil), "sqlflow.Messages")
+	proto.RegisterType((*Columns)(nil), "sqlflow.Columns")
+	proto.RegisterMapType((map[string]*Columns_Column)(nil), "sqlflow.Columns.ColumnsEntry")
+	proto.RegisterType((*Columns_Column)(nil), "sqlflow.Columns.Column")
 }
 
 func init() { proto.RegisterFile("sqlflow.proto", fileDescriptor_6e9f9b081a5698ac) }
 
 var fileDescriptor_6e9f9b081a5698ac = []byte{
-	// 268 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x90, 0x41, 0x4b, 0x03, 0x31,
-	0x10, 0x85, 0xc9, 0xae, 0x76, 0xe9, 0xac, 0xa2, 0xc4, 0x1e, 0xd6, 0x3d, 0xc8, 0xba, 0x20, 0xe4,
-	0x94, 0x4a, 0xbc, 0x88, 0xe2, 0x41, 0x44, 0x2f, 0x7a, 0x31, 0xfe, 0x82, 0x54, 0xd3, 0x3d, 0x34,
-	0x4d, 0xba, 0xcd, 0xc6, 0x92, 0x9f, 0xe5, 0x3f, 0x94, 0x26, 0x46, 0x2a, 0x78, 0xcb, 0xcc, 0xbc,
-	0xf7, 0xf8, 0xf2, 0xe0, 0xd0, 0xf6, 0x6a, 0xae, 0xcc, 0x86, 0xae, 0xd6, 0x66, 0x30, 0xb8, 0xf8,
-	0x19, 0xeb, 0xd3, 0xce, 0x98, 0x4e, 0xc9, 0x69, 0x58, 0xcf, 0xdc, 0x7c, 0x2a, 0xb4, 0x8f, 0x9a,
-	0xf6, 0x0c, 0x80, 0x3b, 0xcd, 0x65, 0xef, 0xa4, 0x1d, 0xf0, 0x31, 0xe4, 0xb6, 0x57, 0x15, 0x6a,
-	0x10, 0x19, 0xf3, 0xed, 0xb3, 0xfd, 0x42, 0x50, 0x06, 0x81, 0x5d, 0x19, 0x6d, 0x25, 0xae, 0xa0,
-	0x58, 0x4a, 0x6b, 0x45, 0x27, 0x2b, 0xd4, 0xe4, 0x64, 0xcc, 0xd3, 0x88, 0x6f, 0xa1, 0x78, 0x37,
-	0xca, 0x2d, 0xb5, 0xad, 0xb2, 0x26, 0x27, 0x25, 0x3b, 0xa7, 0x09, 0x67, 0x27, 0x80, 0x3e, 0x44,
-	0xcd, 0xa3, 0x1e, 0xd6, 0x9e, 0x27, 0x47, 0xfd, 0x0c, 0x07, 0xbb, 0x87, 0x2d, 0xc8, 0x42, 0xfa,
-	0x04, 0xb2, 0x90, 0x1e, 0x5f, 0xc0, 0xfe, 0xa7, 0x50, 0x4e, 0x56, 0x59, 0x83, 0x48, 0xc9, 0x8e,
-	0x7e, 0xc3, 0xa3, 0x8f, 0xc7, 0xeb, 0x4d, 0x76, 0x8d, 0x5a, 0x06, 0xa3, 0xb8, 0xc4, 0x04, 0xf6,
-	0x3e, 0xc4, 0x20, 0x02, 0x6a, 0xc9, 0x26, 0x34, 0xf6, 0x40, 0x53, 0x0f, 0xf4, 0x5e, 0x7b, 0x1e,
-	0x14, 0xec, 0x0e, 0x8a, 0xb7, 0xd7, 0x97, 0x27, 0x65, 0x36, 0x98, 0x41, 0xce, 0x9d, 0xc6, 0x27,
-	0x7f, 0xf1, 0x43, 0x41, 0xf5, 0xe4, 0xbf, 0x3f, 0x5d, 0xa2, 0xd9, 0x28, 0x44, 0x5e, 0x7d, 0x07,
-	0x00, 0x00, 0xff, 0xff, 0xe4, 0xec, 0x9a, 0xa3, 0x82, 0x01, 0x00, 0x00,
+	// 309 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x91, 0xcd, 0x4e, 0xeb, 0x30,
+	0x10, 0x46, 0x9b, 0xf6, 0xde, 0xa6, 0x9d, 0x80, 0x54, 0x4c, 0x25, 0x4a, 0x24, 0x10, 0xca, 0x02,
+	0x65, 0x01, 0x09, 0x0a, 0x0b, 0x10, 0x12, 0x0b, 0x40, 0xa0, 0x2e, 0x60, 0x81, 0xfb, 0x04, 0x29,
+	0xb8, 0x59, 0xd4, 0xb5, 0x9b, 0x38, 0xa6, 0xe4, 0x19, 0x79, 0x29, 0x14, 0xff, 0x24, 0x14, 0x89,
+	0x55, 0xe2, 0x99, 0xa3, 0xef, 0xd8, 0x33, 0xb0, 0x2b, 0x72, 0xba, 0xa0, 0x7c, 0x13, 0xad, 0x0b,
+	0x5e, 0x72, 0xe4, 0x9a, 0xa3, 0x7f, 0x98, 0x71, 0x9e, 0x51, 0x12, 0xab, 0xf2, 0x5c, 0x2e, 0xe2,
+	0x94, 0x55, 0x9a, 0x09, 0x8e, 0x01, 0xb0, 0x64, 0x98, 0xe4, 0x92, 0x88, 0x12, 0x8d, 0xa0, 0x27,
+	0x72, 0x3a, 0x71, 0x4e, 0x9c, 0x70, 0x88, 0xeb, 0xdf, 0xe0, 0x13, 0x3c, 0xd5, 0x17, 0x6b, 0xce,
+	0x04, 0x41, 0x31, 0x0c, 0x56, 0x44, 0x88, 0x34, 0x23, 0x42, 0x51, 0x5e, 0xb2, 0x17, 0x59, 0xe9,
+	0x8b, 0x69, 0x4c, 0x3b, 0xb8, 0x81, 0xd0, 0x19, 0xb8, 0x6f, 0x9c, 0xca, 0x15, 0x13, 0x93, 0xae,
+	0xe2, 0x47, 0x0d, 0xff, 0xa0, 0xeb, 0xd3, 0x0e, 0xb6, 0xc8, 0x3d, 0xc0, 0xa0, 0x30, 0xaa, 0xe0,
+	0x14, 0x06, 0x36, 0x11, 0xf9, 0x5b, 0xda, 0x5e, 0x38, 0x6c, 0x0d, 0xc1, 0x97, 0x03, 0xae, 0x89,
+	0x42, 0x57, 0xad, 0xad, 0xc6, 0xbc, 0xe4, 0xe8, 0xb7, 0xcd, 0x7e, 0x1f, 0x59, 0x59, 0x54, 0x8d,
+	0xd8, 0x9f, 0xc1, 0xce, 0xcf, 0x46, 0x3d, 0x88, 0x25, 0xa9, 0xec, 0x20, 0x96, 0xa4, 0x42, 0xe7,
+	0xf0, 0xff, 0x23, 0xa5, 0x92, 0x98, 0x67, 0x1c, 0xfc, 0x11, 0x8c, 0x35, 0x75, 0xd3, 0xbd, 0x76,
+	0xfc, 0x04, 0xfa, 0xba, 0x88, 0x42, 0xf8, 0xf7, 0x9e, 0x96, 0xa9, 0xb9, 0xd4, 0x38, 0xd2, 0xfb,
+	0x88, 0xec, 0x3e, 0xa2, 0x3b, 0x56, 0x61, 0x45, 0x24, 0xb7, 0xe0, 0xce, 0x5e, 0x9f, 0x9f, 0x28,
+	0xdf, 0xa0, 0x04, 0x7a, 0x58, 0x32, 0xb4, 0xdf, 0x98, 0xda, 0x45, 0xf9, 0xe3, 0xed, 0xa2, 0x1e,
+	0xd9, 0x85, 0x33, 0xef, 0xab, 0xc8, 0xcb, 0xef, 0x00, 0x00, 0x00, 0xff, 0xff, 0x9c, 0x37, 0x9e,
+	0x12, 0x0a, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
